22f86a2d1357ae0e8213e612ea6c0c5619e1e151
joel <joel@deb.casa.org>
2023-12-19 13:04:55 -0500

    move lsp-keymaps to -> core/plugins-keymaps

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -65,13 +65,7 @@ return {
         info = ' '
       })
       lsp_zero.extend_lspconfig()
-
-      lsp_zero.on_attach(function(client, bufnr)
-        -- see :help lsp-zero-keybindings
-        -- to learn the available actions
-        lsp_zero.default_keymaps({buffer = bufnr})
-      end)
-
+      -- LSP keymaps: core/plugins-keymaps/LSP
       require('mason-lspconfig').setup({
         ensure_installed = {},
         handlers = {
