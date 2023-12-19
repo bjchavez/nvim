85e3a1fa7239b88af098fc04b227f16fbe5208ba
joel <joel@deb.casa.org>
2023-12-19 13:06:23 -0500

    remove some unnecessary keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -18,14 +18,6 @@ map({"n", "t"}, "<C-Right>", "<cmd>vertical resize -2<cr>", { desc = "Decrease w
 map("n", "<leader>v", "<cmd>vsplit<cr>", opts)
 map("n", "<leader>h", "<cmd>split<cr>", opts)
 
--- Set shiftwidth
-map("n", "<C-2>", "<cmd>set shiftwidth=2<cr>", opts)
-map("n", "<C-4>", "<cmd>set shiftwidth=4<cr>", opts)
-
--- Mason, Lazy
-map("n", "<leader>m", "<cmd>Mason<cr>", opts)
-map("n", "<leader>l", "<cmd>Lazy<cr>", opts)
-
 -- Buffers
 map("n", "<S-Left>", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
 map("n", "<S-Right>", "<cmd>bnext<cr>", { desc = "Next buffer" })
