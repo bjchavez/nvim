f0e18981548e7f76c7328b342fadc71a65038335
joel <joel@deb.casa.org>
2023-12-22 00:22:20 -0500

    add new plugin: styler by folke

---
+++ lua/plugins/styler.lua
@@ -0,0 +1,11 @@
+return {
+  "folke/styler.nvim",
+  config = function()
+    require("styler").setup({
+      themes = {
+        markdown = { colorscheme = "catppuccin-mocha" },
+        -- help = { colorscheme = "catppuccin-mocha", background = "dark" },
+      },
+    })
+  end
+}
