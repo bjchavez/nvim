d282e826232fcb0b04fc93a730f208687ec412c9
joel <joel@deb.casa.org>
2023-12-23 19:16:34 -0500

    update keymap functionality

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,26 +1,35 @@
-local opts = { noremap = true, silent = true }
 local map = vim.keymap.set
 vim.g.mapleader = " "
 
+local opts = function (desc)
+  if desc ~= nil then
+    return {
+      noremap = true,
+      silent = true,
+      desc = desc
+    }
+  end
+end
+
 -- General
-map( { "n", "i", "v", "s" }, "<C-s>", "<cmd>w<cr>", opts, { desc = "Save file" })
-map("n", "<C-q>", "<cmd>q<cr>", opts)
-map("n", "<C-x>", "<cmd>q!<cr>", opts)
-map("n", "<leader>bd", "<cmd>bdelete<cr>", opts)
-map("n", "<C-h>", "<C-w>h", {desc = "Go to left window"})
-map("n", "<C-j>", "<C-w>j", {desc = "Go to lower window"})
-map("n", "<C-k>", "<C-w>k", {desc = "Go to upper window"})
-map("n", "<C-l>", "<C-w>l", {desc = "Go to right window"})
-map("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
-map("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
-map({"n", "t"}, "<C-Left>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
-map({"n", "t"}, "<C-Right>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
-map("n", "<leader>v", "<cmd>vsplit<cr>", opts)
-map("n", "<leader>h", "<cmd>split<cr>", opts)
+map("n", "<C-s>", "<cmd>w<cr>", opts("Save file"))
+map("n", "<C-q>", "<cmd>q<cr>", opts("Quit"))
+map("n", "<C-x>", "<cmd>q!<cr>", opts("Quit without saving changes"))
+map("n", "<leader>bd", "<cmd>bdelete<cr>", opts("Buffer delete"))
+map("n", "<C-h>", "<C-w>h", opts("Go to left window"))
+map("n", "<C-j>", "<C-w>j", opts("Go to lower window"))
+map("n", "<C-k>", "<C-w>k", opts("Go to upper window"))
+map("n", "<C-l>", "<C-w>l", opts("Go to right window"))
+map("n", "<C-Up>", "<cmd>resize +2<cr>", opts("Increase window height"))
+map("n", "<C-Down>", "<cmd>resize -2<cr>", opts("Decrease window height"))
+map({"n", "t"}, "<C-Left>", "<cmd>vertical resize +2<cr>", opts("Increase window width"))
+map({"n", "t"}, "<C-Right>", "<cmd>vertical resize -2<cr>", opts("Decrease window width"))
+map("n", "<leader>v", "<cmd>vsplit<cr>", opts("Vertical split"))
+map("n", "<leader>h", "<cmd>split<cr>", opts("Horizontal split"))
 
 -- Buffers
-map("n", "<C-,>", "<cmd>bprevious<cr>", { desc = "Previous buffer" })
-map("n", "<C-.>", "<cmd>bnext<cr>", { desc = "Next buffer" })
+map("n", "<C-,>", "<cmd>bprevious<cr>", opts("Previous buffer"))
+map("n", "<C-.>", "<cmd>bnext<cr>", opts("Next buffer"))
 
 -- Clear Search
 map( { "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Clear Search" })
