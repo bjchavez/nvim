b0d4a8f54581756d20866bfc6a04df04c3975875
joel <joel@deb.casa.org>
2023-12-24 19:19:25 -0500

    remove catppuccin colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,34 +1,5 @@
 return {
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    lazy = false,
-    config = function ()
-      require("catppuccin").setup({
-        flavour = "mocha",
-        styles = {
-          comments = { "italic" },
-          types = { "italic" }
-        },
-        integrations = {
-          gitsigns = true,
-          treesitter = true,
-          treesitter_context = true,
-          nvimtree = true,
-          mini = true,
-          indent_blankline = { enable = true },
-          mason = true,
-          markdown = true,
-          symbols_outline = true,
-          telescope = { enabled = true },
-        },
-        no_underline = true
-      })
-      -- vim.cmd[[colorscheme catppuccin]]
-    end
-  },
-  {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
@@ -40,7 +11,13 @@ return {
           keywords = { italic = true }
         }
       })
-      vim.cmd[[colorscheme tokyonight]]
+    end
+  },
+  {
+    "dracula/vim",
+    lazy = false,
+    priority = 1000,
+    config = function()
     end
   }
 }
