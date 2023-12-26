64a949e0e318d0340aca7dc8e9a236cf97b219e1
joel <joel@deb.casa.org>
2023-12-26 12:46:25 -0500

    remove dracula settings

--- lua/plugins/utils/lualine-theme.lua
+++ lua/plugins/utils/lualine-theme.lua
@@ -1,15 +1,4 @@
-local dracula_custom = require "lualine.themes.dracula"
-
 local lualine_utils = {}
-
-lualine_utils.dracula = function ()
-  dracula_custom.normal.c.bg = ""
-  dracula_custom.insert.c.bg = ""
-  dracula_custom.visual.c.bg = ""
-  dracula_custom.replace.c.bg = ""
-  dracula_custom.inactive.c.bg = ""
-  return dracula_custom
-end
 
 lualine_utils.separators = {
   default = { right = "", left = "" },
