1e30c49d2cafc6d3e81e7fe363e40b4f83c10a56
joel <joel@deb.casa.org>
2023-12-28 10:44:21 -0500

    update telescope plugin version

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
   'nvim-telescope/telescope.nvim',
-  tag = '0.1.2',
+  tag = '0.1.5',
   dependencies = { 'nvim-lua/plenary.nvim' },
   opts = function()
     return require "plugins.configs.telescope-config"
