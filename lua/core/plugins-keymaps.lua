32d7c9d35f3c32f31134a6119b14263aa3522f08
joel <joel@deb.casa.org>
2023-12-27 22:36:25 -0500

    add toggleterm plugin keymaps

--- lua/core/plugins-keymaps.lua
+++ lua/core/plugins-keymaps.lua
@@ -1,20 +1,29 @@
-local opts = { noremap = true, silent = true }
 local map = vim.keymap.set
 vim.g.mapleader = " "
 
+local opts = function (title, desc)
+  if title ~= nil and desc ~= nil then
+    return {
+      noremap = true,
+      silent = true,
+      desc = title .. ": " .. desc
+    }
+  end
+end
+
 -- Telescope
 local telescope = require("telescope.builtin")
-map("n", "<leader>ff", telescope.find_files, { desc = "Find files" })
-map("n", "<leader>bf", telescope.buffers, { desc = "Buffers" })
-map("n", "<leader>fg", telescope.help_tags, { desc = "Help Tags" })
-map("n", "<leader>gs", telescope.git_status, { desc = "Git Status" })
-map("n", "<leader>gc", telescope.git_commits, { desc = "Git Commits" })
-map("n", "<leader>t", "<cmd>Telescope<cr>")
-map("n", "<leader>fb", "<cmd>Telescope file_browser<cr>", opts)
+map("n", "<leader>ff", telescope.find_files, opts("Telescope", "Find Files"))
+map("n", "<leader>bf", telescope.buffers, opts("Telescope", "Show Buffers"))
+map("n", "<leader>fg", telescope.help_tags, opts("Telescope", "Help Tags"))
+map("n", "<leader>gs", telescope.git_status, opts("Telescope", "Git Status"))
+map("n", "<leader>gc", telescope.git_commits, opts("Telescope", "Git Commits"))
+map("n", "<leader>t", "<cmd>Telescope<cr>", opts("Telescope", "Show Telescope"))
+map("n", "<leader>fb", "<cmd>Telescope file_browser<cr>", opts("Telescope", "File Browser"))
 
 -- Markdown Preview
-map("n", "<leader>mp", "<cmd>MarkdownPreview<cr>", opts, { desc = "Start Markdown Preview" })
-map("n", "<leader>ms", "<cmd>MarkdownPreviewStop<cr>", opts, { desc = "Stop Markdown Preview" })
+map("n", "<leader>mp", "<cmd>MarkdownPreview<cr>", opts("Markdown", "Start Markdown Preview"))
+map("n", "<leader>ms", "<cmd>MarkdownPreviewStop<cr>", opts("Markdown", "Stop Markdown Preview"))
 
 -- LSP
 local lsp_zero = require("lsp-zero")
@@ -25,3 +34,7 @@ lsp_zero.on_attach(function(client, bufnr)
   vim.keymap.set("n", "<leader>n", vim.diagnostic.goto_next, lsp_opts)
   vim.keymap.set("n", "<leader>m", vim.diagnostic.goto_prev, lsp_opts)
 end)
+
+-- Toggleterm
+map("n", "<A-i>", "<cmd>ToggleTerm direction=float<cr>", opts("ToggleTerm", "Show Float Terminal"))
+map("t", "<A-i>", "<cmd>ToggleTerm direction=float<cr>", opts("ToggleTerm", "Hide Float Terminal"))
