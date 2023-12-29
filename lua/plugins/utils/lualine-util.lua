a767a04d80d42c14778deb6ebea628ac8ab1c825
joel <joel@deb.casa.org>
2023-12-28 20:41:02 -0500

    rename lualine-theme to -> lualine-util

---
+++ lua/plugins/utils/lualine-util.lua
@@ -0,0 +1,11 @@
+local lualine_utils = {}
+
+lualine_utils.separators = {
+  default = { right = "", left = "" },
+  block = { right = "█", left = "█" },
+  arrow = { right = "", left = "" },
+  triangle = { left = "", right = "" },
+}
+
+return lualine_utils
+
