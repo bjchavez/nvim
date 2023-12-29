521d8e1616a88fb41f95101fc8ad75754caef95c
joel <joel@deb.casa.org>
2023-12-28 20:43:14 -0500

    remove nvim-lspconfig keymap

--- lua/core/plugins-keymaps.lua
+++ lua/core/plugins-keymaps.lua
@@ -26,16 +26,6 @@ map("n", "<leader>fb", "<cmd>Telescope file_browser<cr>", opts("Telescope", "Fil
 map("n", "<leader>mp", "<cmd>MarkdownPreview<cr>", opts("Markdown", "Start markdown preview"))
 map("n", "<leader>ms", "<cmd>MarkdownPreviewStop<cr>", opts("Markdown", "Stop markdown preview"))
 
--- LSP
-local lsp_zero = require("lsp-zero")
-lsp_zero.on_attach(function(client, bufnr)
-  local lsp_opts = { buffer = bufnr, remap = false }
-  vim.keymap.set("n", "gd", vim.lsp.buf.definition, lsp_opts)
-  vim.keymap.set("n", "gh", vim.lsp.buf.hover, lsp_opts)
-  vim.keymap.set("n", "<leader>n", vim.diagnostic.goto_next, lsp_opts)
-  vim.keymap.set("n", "<leader>m", vim.diagnostic.goto_prev, lsp_opts)
-end)
-
 -- Toggleterm
 map("n", "<A-i>", "<cmd>ToggleTerm direction=float<cr>", opts("ToggleTerm", "Show float terminal"))
 map("t", "<A-i>", "<cmd>ToggleTerm direction=float<cr>", opts("ToggleTerm", "Hide float terminal"))
