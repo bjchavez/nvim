699dfccb9dc658f5cae9861af0877b0950acaa20
joel <joel@deb.casa.org>
2023-12-31 01:17:43 -0500

    add new mini plugins

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -2,15 +2,13 @@ return {
   "echasnovski/mini.nvim",
   version = "*",
   opts = function ()
-    local conf = require "plugins.configs.mini-config"
-    return conf
+    return require "plugins.configs.mini-config"
   end,
-  config = function(_,conf)
-    require('mini.comment').setup(conf.comment)
-    require('mini.ai').setup()
+  config = function(_,opts)
+    require('mini.comment').setup(opts.comment)
     require('mini.cursorword').setup()
     require('mini.pairs').setup()
     require('mini.trailspace').setup()
-    require('mini.surround').setup()
+    require('mini.move').setup()
   end
 }
