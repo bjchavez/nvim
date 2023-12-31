565599d7edef66d689d9f0f1f526c562040f972a
joel <joel@deb.casa.org>
2023-12-30 22:03:26 -0500

    update plugins keymap

--- lua/core/plugins-keymaps.lua
+++ lua/core/plugins-keymaps.lua
@@ -1,5 +1,4 @@
-local map = vim.keymap.set
-
+-- NOTE: Feature to automate keymap options table
 local options = function (title, desc)
   if title ~= nil and desc ~= nil then
     return {
@@ -10,21 +9,21 @@ local options = function (title, desc)
   end
 end
 
--- Telescope
+-- NOTE: Telescope
 local telescope = require("telescope.builtin")
-map("n", "<leader>ff", telescope.find_files, options("Telescope", "Find files"))
-map("n", "<leader>bf", telescope.buffers, options("Telescope", "Show buffers"))
-map("n", "<leader>fg", telescope.help_tags, options("Telescope", "Help tags"))
-map("n", "<leader>gs", telescope.git_status, options("Telescope", "Git status"))
-map("n", "<leader>gc", telescope.git_commits, options("Telescope", "Git commits"))
-map("n", "<leader>km", telescope.keymaps, options("Telescope", "Show keymaps"))
-map("n", "<leader>t", "<cmd>Telescope<cr>", options("Telescope", "Show telescope"))
-map("n", "<leader>fb", "<cmd>Telescope file_browser<cr>", options("Telescope", "File browser"))
+vim.keymap.set("n", "<leader>ff", telescope.find_files, options("Telescope", "Find files"))
+vim.keymap.set("n", "<leader>bf", telescope.buffers, options("Telescope", "Show buffers"))
+vim.keymap.set("n", "<leader>fg", telescope.help_tags, options("Telescope", "Help tags"))
+vim.keymap.set("n", "<leader>gs", telescope.git_status, options("Telescope", "Git status"))
+vim.keymap.set("n", "<leader>gc", telescope.git_commits, options("Telescope", "Git commits"))
+vim.keymap.set("n", "<leader>km", telescope.keymaps, options("Telescope", "Show keymaps"))
+vim.keymap.set("n", "<leader>t", "<cmd>Telescope<cr>", options("Telescope", "Show telescope"))
+vim.keymap.set("n", "<leader>fb", "<cmd>Telescope file_browser<cr>", options("Telescope", "File browser"))
 
--- Markdown Preview
-map("n", "<leader>mp", "<cmd>MarkdownPreview<cr>", options("Markdown Preview", "Start markdown preview"))
-map("n", "<leader>ms", "<cmd>MarkdownPreviewStop<cr>", options("Markdown Preview", "Stop markdown preview"))
+-- NOTE:  Markdown Preview
+vim.keymap.set("n", "<leader>mp", "<cmd>MarkdownPreview<cr>", options("Markdown Preview", "Start markdown preview"))
+vim.keymap.set("n", "<leader>ms", "<cmd>MarkdownPreviewStop<cr>", options("Markdown Preview", "Stop markdown preview"))
 
--- Toggleterm
-map("n", "<A-i>", "<cmd>ToggleTerm direction=float<cr>", options("ToggleTerm", "Show float terminal"))
-map("t", "<A-i>", "<cmd>ToggleTerm direction=float<cr>", options("ToggleTerm", "Hide float terminal"))
+-- NOTE:  Toggleterm
+vim.keymap.set("n", "<A-i>", "<cmd>ToggleTerm direction=float<cr>", options("ToggleTerm", "Show float terminal"))
+vim.keymap.set("t", "<A-i>", "<cmd>ToggleTerm direction=float<cr>", options("ToggleTerm", "Hide float terminal"))
