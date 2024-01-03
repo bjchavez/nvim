885429d5e71c0b27b1101f213ba60751fa36a3a2
joel <joel@deb.casa.org>
2024-01-02 22:07:01 -0500

    add lazy keys for telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,65 @@
 return {
   'nvim-telescope/telescope.nvim',
   tag = '0.1.5',
-  dependencies = { 'nvim-lua/plenary.nvim' },
+  keys = {
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    {
+      "<leader>ff",
+      function ()
+        local builtin = require("telescope.builtin")
+        builtin.find_files({
+          no_ignore = false,
+          hidden = true
+        })
+      end,
+      desc = "Telescope: Find files"
+    },
+    {
+      "<leader>bf",
+      function ()
+        local builtin = require("telescope.builtin")
+        builtin.buffers()
+      end,
+      desc = "Telescope: Show buffers"
+    },
+    {
+      "<leader>fg",
+      function ()
+        local builtin = require("telescope.builtin")
+        builtin.help_tags()
+      end,
+      desc = "Telescope: Help tags"
+    },
+    {
+      "<leader>gs",
+      function ()
+        local builtin = require("telescope.builtin")
+        builtin.git_status()
+      end,
+      desc = "Telescope: Git status"
+    },
+    {
+      "<leader>gc",
+      function ()
+        local builtin = require("telescope.builtin")
+        builtin.git_commits()
+      end,
+      desc = "Telescope: Git commits"
+    },
+    {
+      "<leader>km",
+      function ()
+        local builtin = require("telescope.builtin")
+        builtin.keymaps()
+      end,
+      desc = "Telescope: Keymaps"
+    },
+    {
+      "<leader>tl",
+      "<cmd>Telescope<cr>",
+      desc = "Telescope"
+    },
+  },
   opts = function()
     return require "plugins.configs.telescope-config"
   end,
