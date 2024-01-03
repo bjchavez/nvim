a4268f74e0327814e6882bda07f56e67c93c7f37
joel <joel@deb.casa.org>
2024-01-02 22:06:32 -0500

    add lazy keys for markdown preview

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,6 +1,17 @@
 return {
   "iamcco/markdown-preview.nvim",
-  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
   ft = { "markdown" },
   build = function() vim.fn["mkdp#util#install"]() end,
+  keys = {
+    {
+      "<leader>mp",
+      "<cmd>MarkdownPreview<cr>",
+      desc = "Markdown: Start markdown preview"
+    },
+    {
+      "<leader>ms",
+      "<cmd>MarkdownPreviewStop<cr>",
+      desc = "Markdown: Stop markdown preview"
+    }
+  }
 }
