5702cc6cf67520ac2e2546751ba3008785a87684
joel <joel@deb.casa.org>
2024-01-02 22:07:33 -0500

    add lazy keys for toggleterm

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,6 +1,20 @@
 return {
   'akinsho/toggleterm.nvim',
   version = "*",
+  keys = {
+    {
+      "<A-i>",
+      "<cmd>ToggleTerm direction=float<cr>",
+      desc = "ToggleTerm: Show float terminal",
+      mode = { "n" }
+    },
+    {
+      "<A-i>",
+      "<cmd>ToggleTerm direction=float<cr>",
+      desc = "ToggleTerm: Hide float terminal",
+      mode = { "t" }
+    }
+  },
   opts = function ()
     return require "plugins.configs.toggleterm-config"
   end,
